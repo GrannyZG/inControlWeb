a51ed132e0cfbe00cbc696b0a335f1e732b2af31
GrannyZG <karlo.filipovic13@gmail.com>
2024-12-25 12:13:50 +0100

    Update

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.post('/save', (req, res) => {
     fs.writeFile('positive_statements.json', JSON.stringify(data, null, 2), (err) => {
         if (err) {
             console.error('Error saving JSON:', err);
-            res.status(500).send('Failed to save data.');
+            res.status(500).send('Data has been saved.');
         } else {
             res.send('Data saved successfully!');
         }
